9492d28b71d78e73b31c8e88dadc9f6fb1b0552b
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-04-20 12:11:25 +0530

    Clean

--- src/main/java/com/example/Main_Class_Java_Leet_Code_Exercises.java
+++ src/main/java/com/example/Main_Class_Java_Leet_Code_Exercises.java
@@ -1,10 +1,6 @@
 package com.example;
 
-import java.util.Arrays;
-import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main_Class_Java_Leet_Code_Exercises {
     public static void main() {
 
